ca9ee6c492a2760cc1bca6cc86fe0af0bdac57b3
Tsai <johnsan11111@gmail.com>
2019-01-07 15:19:57 +0800

    1123
    12

--- 123/main.c
+++ 123/main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<time,h>
 
 int main()
 {
+srand(time(NULL));
+
+int i;
+
+i=rand()%6+1;
+
     printf("Hello world!\n");
 
-    printf("Hey stupid guy!\n";)
+    printf("Hey stupid idiot!\n");
+
+    printf("%d",i);
+
+    printf("GG in in der");
+
+
     return 0;
 }
